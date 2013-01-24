c9114d869b61a2dcb3b3e35657cdf409904b6e8f
cloud <owenc4a4@qq.com>
2013-01-24 14:10:33 +0800

    upload-win

---
+++ tools/upload-win.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import os
+import sys
+
+pwd_tmp = os.getcwd()
+script_dir = os.path.dirname(__file__)
+if sys.platform == 'cygwin':
+  nw_dir = '/home/Administrator/node-webkit/content/nw/tools'
+  nw_build_dir = '/home/Administrator/node-webkit/out/Release'
+
+elif sys.platform == 'win32':
+  nw_dir = 'E:\\cygwin\\home\\Administrator\\code\\node-webkit\\src\\content\\nw\\tools'
+  nw_build_dir = 'E:\\cygwin\\home\\Administrator\\code\\node-webkit\\src\\out\\Release'
+else:
+  quit()
+
+if sys.platform != 'cygwin':
+  quit()
+  
+sys.path.insert(0, nw_dir)
+import getnwisrelease
+import getnwversion
+
+nw_version = 'v' + getnwversion.nw_version
+is_release = getnwisrelease.release
+
+if is_release == 0:
+  nw_version += '-pre'
+
+binary_tar = 'node-webkit-' + nw_version + '-win-ia32.zip'
+require_files = ['nw.exp', 'nw.lib', 'nw.pdb']
+os.chdir(script_dir)
+
+if not os.path.isdir('forupload'):
+  os.mkdir('forupload')
+
+os.chdir('forupload')
+
+if not os.path.isdir(nw_version):
+  os.mkdir(nw_version)
+
+print 'copy files.'  
+import shutil
+for file in require_files:
+  shutil.copy(os.path.join(nw_build_dir, file),
+              os.path.join(nw_version, file))
+
+shutil.copy(os.path.join(nw_build_dir, 'node-webkit-binaries', binary_tar),
+              os.path.join(nw_version, binary_tar))
+
+os.system('scp -r %s owen-cloud@10.0.2.2:/home/owen-cloud/release' 
+          %(nw_version))
+
+os.chdir(pwd_tmp)
